5a9406066a75c676698f9c8991e9d9e9acc9fe04
Diogo Dantas <diogo.dantas@live.com>
2024-01-22 15:48:01 -0300

    End of exercises relating to the topic of time between events.

--- practice/datacamp/eda#dates_and_timestamps.sql
+++ practice/datacamp/eda#dates_and_timestamps.sql
@@ -194,3 +194,88 @@ SELECT date_trunc('month', date) AS month,
        ON all_days.date=daily_count.day
  GROUP BY month
  ORDER BY month; 
+
+ --Longest gap
+
+-- Compute the gaps
+WITH request_gaps AS (
+        SELECT date_created,
+               -- lead or lag
+               lag(date_created) OVER (ORDER BY date_created) AS previous,
+               -- compute gap as date_created minus lead or lag
+               date_created - lag(date_created) OVER (ORDER BY date_created) AS gap
+          FROM evanston311)
+-- Select the row with the maximum gap
+SELECT *
+  FROM request_gaps
+-- Subquery to select maximum gap from request_gaps
+ WHERE gap = (SELECT MAX(gap)
+                FROM request_gaps);
+
+ -- Rats!
+
+ -- Truncate the time to complete requests to the day
+SELECT date_trunc('day', date_completed - date_created) AS completion_time,
+-- Count requests with each truncated time
+       count(*)
+  FROM evanston311
+-- Where category is rats
+ WHERE category = 'Rodents- Rats'
+-- Group and order by the variable of interest
+ GROUP BY completion_time
+ ORDER BY count;
+
+ SELECT category, 
+       -- Compute average completion time per category
+       AVG(date_completed - date_created) AS avg_completion_time
+  FROM evanston311
+-- Where completion time is less than the 95th percentile value
+ WHERE (date_completed - date_created) < 
+-- Compute the 95th percentile of completion time in a subquery
+         (SELECT percentile_disc(0.95) WITHIN GROUP (
+          ORDER BY (date_completed - date_created))
+            FROM evanston311)
+ GROUP BY category
+-- Order the results
+ ORDER BY avg_completion_time DESC;
+
+ -- Compute correlation (corr) between 
+-- avg_completion time and count from the subquery
+SELECT corr(avg_completion, count)
+  -- Convert date_created to its month with date_trunc
+  FROM (SELECT date_trunc('month', date_created) AS month, 
+               -- Compute average completion time in number of seconds           
+               avg(EXTRACT(epoch FROM date_completed - date_created)) AS avg_completion, 
+               -- Count requests per month
+               count(*) AS count
+          FROM evanston311
+         -- Limit to rodents
+         WHERE category='Rodents- Rats' 
+         -- Group by month, created above
+         GROUP BY month) 
+         -- Required alias for subquery 
+         AS monthly_avgs;
+
+-- Compute monthly counts of requests created
+WITH created AS (
+       SELECT date_trunc('month',date_created) AS month,
+              count(*) AS created_count
+         FROM evanston311
+        WHERE category='Rodents- Rats'
+        GROUP BY month),
+-- Compute monthly counts of requests completed
+      completed AS (
+       SELECT date_trunc('month',date_completed)  AS month,
+              count(*) AS completed_count
+         FROM evanston311
+        WHERE category='Rodents- Rats'
+        GROUP BY month)
+-- Join monthly created and completed counts
+SELECT created.month, 
+       created_count, 
+       completed_count
+  FROM created
+       INNER JOIN completed
+       ON created.month=completed.month
+ ORDER BY created.month;
+
