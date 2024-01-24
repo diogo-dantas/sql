e62bdfa9dde415d80e4b41c3d3b404ae587d7900
Diogo Dantas <diogo.dantas@live.com>
2024-01-23 22:33:37 -0300

    End of exercises related to the topic 'Selecting Data.'

--- practice/datacamp/intermediate_sql#selecting_data.sql
+++ practice/datacamp/intermediate_sql#selecting_data.sql
@@ -12,5 +12,34 @@ FROM people;
 SELECT COUNT(country) AS count_countries, COUNT(language) AS count_languages
 FROM films;
 
+-- SELECT DISTINCT
 
+-- Return the unique countries from the films table
+SELECT DISTINCT country
+FROM films;
+
+-- Count the distinct countries from the films table
+SELECT COUNT(DISTINCT country) AS count_distinct_countries
+FROM films;
+
+-- Debugging errors
+
+-- Debug this code
+SELECT certification
+FROM films
+LIMIT 5;
+
+-- Debug this code
+SELECT film_id, imdb_score, num_votes
+FROM reviews;
+
+SELECT COUNT(birthdate) AS count_birthdays
+FROM people;
+
+-- Formatting
+
+-- Rewrite this query
+SELECT person_id, role 
+FROM roles
+LIMIT 10;
 
