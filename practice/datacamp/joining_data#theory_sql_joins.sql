01eee594abd3cf9041b79d4b9f27752c71555fb9
Diogo Dantas <diogo.dantas@live.com>
2024-01-26 11:56:15 -0300

    End of exercises related to the topic about using set theory operations in SQL, with an introduction to UNION, UNION ALL, INTERSECT, and EXCEPT clauses.

---
+++ practice/datacamp/joining_data#theory_sql_joins.sql
@@ -0,0 +1,55 @@
+-- Set Theory for SQL Joins
+
+--Comparing global economies
+
+-- Select all fields from economies2015
+SELECT *
+FROM economies2015
+-- Set operation
+UNION
+-- Select all fields from economies2019
+SELECT * 
+FROM economies2019
+ORDER BY code, year;
+
+--Comparing two set operations
+
+-- Query that determines all pairs of code and year from economies and populations, without duplicates
+SELECT code, year
+FROM economies
+UNION 
+SELECT country_code, year
+FROM populations
+ORDER BY code, year;
+
+SELECT code, year
+FROM economies
+-- Set theory clause
+UNION ALL
+SELECT country_code, year
+FROM populations
+ORDER BY code, year;
+
+-- At the INTERSECT
+
+--INTERSECT
+
+-- Return all cities with the same name as a country
+SELECT name
+FROM cities
+INTERSECT
+SELECT name
+FROM countries;
+
+-- EXCEPT
+
+--You've got it, EXCEPT...
+
+-- Return all cities that do not have the same name as a country
+SELECT name
+FROM cities
+EXCEPT
+SELECT name
+FROM countries
+ORDER BY name;
+
